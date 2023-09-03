47a9712308502ca86b541bf1fb727b43a38b2630
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2023-09-04 00:57:21 +0300

    update css

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -66,12 +66,14 @@ export const ContactForm = () => {
         {/* <FcAddDatabase size={'35px'} className={css.icon} /> */}
         <label className={css.label}>
           Name
-          <Field
+          <div className={css.inputWrap}>
+                <Field
             className={css.input}
             name="name"
             // pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           />
+      </div>
           <ErrorMessage
             component="div"
             className={css.error_name}
@@ -80,12 +82,14 @@ export const ContactForm = () => {
         </label>
         <label className={css.label}>
           Number
-          <Field
+          <div className={css.inputWrap}>
+             <Field
             className={css.input}
             name="number"
             // pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           />
+         </div>
         </label>
         <ErrorMessage
           component="div"
